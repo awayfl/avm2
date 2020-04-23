50b3fb2bd3e90a5dcdd3664e34fb3f699fb056db
Exponenta <rondo.devil@gmail.com>
2020-04-23 23:13:10 +0300

    register writers debug api

--- lib/run/writers.ts
+++ lib/run/writers.ts
@@ -1,4 +1,4 @@
-import { IndentingWriter, dumpLine } from "@awayfl/swf-loader";
+import { IndentingWriter, dumpLine, release, registerDebugMethod } from "@awayfl/swf-loader";
 import { WriterFlags } from "./WriterFlags";
 
 var writer = new IndentingWriter(false, function (x) { dumpLine(x); } );
@@ -10,8 +10,21 @@ export function sliceArguments(args, offset: number) {
   return Array.prototype.slice.call(args, offset);
 }
 
-export function setWriters(flags: WriterFlags) {
-  runtimeWriter = (flags & WriterFlags.Runtime) ? writer : null;
-  executionWriter = (flags & (WriterFlags.Execution | WriterFlags.Interpreter)) ? writer : null;
-  interpreterWriter = (flags & WriterFlags.Interpreter) ? writer : null;
+export function setWriters(flags: WriterFlags, output: IndentingWriter | Function | null = writer) {
+  if(typeof (<any>output) === 'function') {
+    output = new IndentingWriter(false, output)
+  }
+
+  runtimeWriter = (flags & WriterFlags.Runtime) ? output : null;
+  executionWriter = (flags & (WriterFlags.Execution | WriterFlags.Interpreter)) ? output : null;
+  interpreterWriter = (flags & WriterFlags.Interpreter) ? output : null;
+}
+
+// export WRITER API for capture AVM2 reports
+if(!release) {
+	 registerDebugMethod(setWriters, { 
+      name: "registerWriter", 
+      description:"Registering a writer function for capturing logs", 
+      declaration: [{name:"flags", type: "string"}, {name: "func", type:"function" }] 
+    })
 }
