9261369b96b47357b5ab9cce0933f3dd51e551a3
eXponenta <rondo.devil@gmail.com>
2020-05-23 18:17:55 +0300

    update version number in index.ts

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-console.debug("AwayFL - AVM2 - 0.2.41");
+console.debug("AwayFL - AVM2 - 0.2.42");
 
 // export {AVMAwayStage} from "./lib/AVMAwayStage";
 export { ASObject } from "./lib/nat/ASObject";
