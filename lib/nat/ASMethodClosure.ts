85f9e55efcc133461a3233a2ddbbed3c2a9a9b45
Exponenta <rondo.devil@gmail.com>
2020-05-26 23:43:59 +0300

    experimental callable improvements - reduce callers

--- lib/nat/ASMethodClosure.ts
+++ lib/nat/ASMethodClosure.ts
@@ -13,14 +13,30 @@ export class ASMethodClosure extends ASFunction {
       defineNonEnumerableProperty(proto, '$Bgcall', asProto.call);
       defineNonEnumerableProperty(proto, '$Bgapply', asProto.apply);
     }
+
+    private _value: AXCallable;
+
     static Create(receiver: AXObject, method: AXCallable) {
       var closure: ASMethodClosure = Object.create(this.sec.AXMethodClosure.tPrototype);
-      closure.receiver = <any>receiver;
-      closure.value = method;
+
+      closure._value = (<any>method);
       closure.methodInfo = method.methodInfo;
+      
+      closure.setReceiver(receiver);
+
       return closure;
     }
   
+    setReceiver(r: any) {
+      if(this.receiver === r) {
+        return;
+      }
+
+      this.receiver = r;
+      //@ts-ignore
+      this.value = this._value.bind(r);
+    }
+
     get prototype(): AXObject {
       return null;
     }
@@ -29,20 +45,37 @@ export class ASMethodClosure extends ASFunction {
       this.sec.throwError("ReferenceError", Errors.ConstWriteError, "prototype",
                                       "MethodClosure");
     }
-  
-    axCall(ignoredThisArg: any): any {
-      return this.value.apply(this.receiver, sliceArguments(arguments, 1));
+
+    //@ts-ignore
+    axCall(ignoredThisArg: any, arg1:any, arg2:any, arg3:any, arg4:any, arg5:any, arg6:any, arg7:any, arg8:any): any {
+      //@ts-ignore
+      return this.value(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8);
     }
   
     axApply(ignoredThisArg: any, argArray?: any[]): any {
-      return this.value.apply(this.receiver, argArray);
+      if(!argArray || argArray.length === 0) {
+        //@ts-ignore
+        this.value()
+      }
+      //@ts-ignore
+      return this.value(argArray[0], argArray[1], argArray[2], argArray[3], argArray[4], argArray[5], argArray[6], argArray[7]);
     }
   
-    call(ignoredThisArg: any) {
-      return this.value.apply(this.receiver, sliceArguments(arguments, 1));
+    //@ts-ignore
+    call(ignoredThisArg: any, arg1:any, arg2:any, arg3:any, arg4:any, arg5:any, arg6:any, arg7:any, arg8:any) {
+      //@ts-ignore
+      return this.value(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8);
     }
   
-    apply(ignoredThisArg: any, argArray?: ASArray): any {
-      return this.value.apply(this.receiver, argArray ? argArray.value : undefined);
+    apply(ignoredThisArg: any, argArrayAS?: ASArray): any {
+      const  argArray = argArrayAS ? argArrayAS.value : undefined;
+
+      if(!argArray || argArray.length === 0) {
+        //@ts-ignore
+        this.value();
+      }
+      
+      //@ts-ignore
+      return this.value(argArray[0], argArray[1], argArray[2], argArray[3], argArray[4], argArray[5], argArray[6], argArray[7]);
     }
   }
