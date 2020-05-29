2a6167ff31226db2b88c1dccf86c49cb0bf2db14
Rob Bateman <rob.bateman@gmail.com>
2020-05-29 12:17:28 +0100

    update version number in index.ts

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-console.debug("AwayFL - AVM2 - 0.2.46");
+console.debug("AwayFL - AVM2 - 0.2.47");
 
 // export {AVMAwayStage} from "./lib/AVMAwayStage";
 export { ASObject } from "./lib/nat/ASObject";
