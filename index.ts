61e0fe1ba59f0383b637593489152b0316022f5a
Konstantin Timoshenko <rondo.devil@gmail.com>
2020-07-21 21:41:49 +0300

    update version number in index.ts

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-console.debug("AwayFL - AVM2 - 0.2.77");
+console.debug("AwayFL - AVM2 - 0.2.78");
 
 // export {AVMAwayStage} from "./lib/AVMAwayStage";
 export { ASObject } from "./lib/nat/ASObject";
