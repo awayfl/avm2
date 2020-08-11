1933b3e00db0cf4fb085b0328c0ec815d86f05a9
80prozent <80prozent@differentdesign.de>
2020-08-11 22:32:47 +0200

    update version number in index.ts

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-console.debug("AwayFL - AVM2 - 0.2.91");
+console.debug("AwayFL - AVM2 - 0.2.92");
 
 // export {AVMAwayStage} from "./lib/AVMAwayStage";
 export { ASObject } from "./lib/nat/ASObject";
