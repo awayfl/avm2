d66ebc33989cff297f9e48603fea60234f21c472
Exponenta <rondo.devil@gmail.com>
2020-11-09 20:35:37 +0300

    enforce native implementation

--- lib/nat/builtinNativeClasses.ts
+++ lib/nat/builtinNativeClasses.ts
@@ -2,9 +2,9 @@ import { MapObject, ObjectUtilities } from '@awayfl/swf-loader';
 
 import { ASClass } from './ASClass';
 import { NamespaceType } from '../abc/lazy/NamespaceType';
-export var builtinNativeClasses: MapObject<ASClass> = ObjectUtilities.createMap<ASClass>();
-export var nativeClasses: MapObject<ASClass> = ObjectUtilities.createMap<ASClass>();
-export var nativeClassLoaderNames: {
+export const builtinNativeClasses: MapObject<typeof ASClass> = ObjectUtilities.createMap<typeof ASClass>();
+export const nativeClasses: MapObject<typeof ASClass> = ObjectUtilities.createMap<typeof ASClass>();
+export const nativeClassLoaderNames: {
 	name: string;
 	alias: string;
 	nsType: NamespaceType
--- lib/nat/getNativeInitializer.ts
+++ lib/nat/getNativeInitializer.ts
@@ -6,7 +6,7 @@ export function getNativeInitializer(classInfo: ClassInfo): AXCallable {
 	const methodInfo = classInfo.instanceInfo.getInitializer();
 	const className = classInfo.instanceInfo.getClassName();
 	const asClass = builtinNativeClasses[className] || nativeClasses[className];
-	if (methodInfo.isNative()) {
+	if (methodInfo.isNative() || asClass?.forceNative) {
 		// Use TS constructor as the initializer function.
 		return <any>asClass;
 	}
