awayfl/avm2

commit 8386d9061de05d590b4df2ba5944b8e559cad050

tidy up Traits parsing and multiname caching

lib/jit.ts
@@ -150,8 +150,6 @@ function isFastReturnVoid(
 self.attach_hook = UNSAFE_attachMethodHook;
 
 function resolveTrait(info: InstanceInfo, name: Multiname): TraitInfo | RuntimeTraitInfo {
-	info.traits.resolve();
-
 	let trait = info.traits.getTrait(name);
 	let superInstance = info;
 
@@ -175,7 +173,6 @@ function resolveTrait(info: InstanceInfo, name: Multiname): TraitInfo | RuntimeT
 			}
 		}
 
-		superInstance.traits.resolve();
 		trait = superInstance.traits.getTrait(name);
 
 		if (trait) {
@@ -2127,7 +2124,7 @@ export class Context {
 	}
 
 	call(value: AXCallable, obj: ASObject, pp: any[], scope: Scope = null): any {
-		if (scope && (<any>value.methodInfo?.trait?.name)?.name == 'getDefinitionByName') {
+		if (scope && value.methodInfo.trait.multiname.name == 'getDefinitionByName') {
 			return this.getdefinitionbyname(scope, obj, pp);
 		}
 		validateCall(this.sec, value, pp.length);

lib/abc/lazy/ABCFile.ts
@@ -468,7 +468,7 @@ export class ABCFile {
 
 	private _parseTrait() {
 		const s = this._stream;
-		const name = s.readU30();
+		const nameIndex = s.readU30();
 		const tag = s.readU8();
 
 		const kind = tag & 0x0F;
@@ -479,13 +479,13 @@ export class ABCFile {
 			case TRAIT.Slot:
 			case TRAIT.Const:
 				var slot = s.readU30();
-				var type = s.readU30();
+				var typeIndex = s.readU30();
 				var valueIndex = s.readU30();
 				var valueKind = -1;
 				if (valueIndex !== 0) {
 					valueKind = s.readU8();
 				}
-				trait = new SlotTraitInfo(this, kind, name, slot, type, valueKind, valueIndex);
+				trait = new SlotTraitInfo(this, kind, this.getMultiname(nameIndex), slot, this.getMultiname(typeIndex), valueKind, valueIndex);
 				break;
 			case TRAIT.Method:
 			case TRAIT.Getter:
@@ -493,12 +493,12 @@ export class ABCFile {
 				var dispID = s.readU30(); // Tamarin optimization.
 				var methodInfoIndex = s.readU30();
 				var methodInfo = this.getMethodInfo(methodInfoIndex);
-				trait = methodInfo.trait = new MethodTraitInfo(this, kind, name, methodInfo);
+				trait = methodInfo.trait = new MethodTraitInfo(this, kind, this.getMultiname(nameIndex), methodInfo);
 				break;
 			case TRAIT.Class:
 				var slot = s.readU30();
 				var classInfo = this.classes[s.readU30()];
-				trait = classInfo.trait = new ClassTraitInfo(this, kind, name, slot, classInfo);
+				trait = classInfo.trait = new ClassTraitInfo(this, kind, this.getMultiname(nameIndex), slot, classInfo);
 				break;
 			default:
 				this.applicationDomain.sec.throwError('VerifierError',
@@ -580,9 +580,9 @@ export class ABCFile {
 		const start = s.readU30();
 		const end = s.readU30();
 		const target = s.readU30();
-		const type = s.readU30();
-		const varName = s.readU30();
-		return new ExceptionInfo(this, start, end, target, type, varName);
+		const typeIndex = s.readU30();
+		const nameIndex = s.readU30();
+		return new ExceptionInfo(this, start, end, target, this.getMultiname(nameIndex), this.getMultiname(typeIndex));
 	}
 
 	public getConstant(kind: CONSTANT, i: number): any {

lib/abc/lazy/ClassInfo.ts
@@ -17,7 +17,7 @@ export class ClassInfo extends Info {
 		public initializer: MethodInfo | number,
 		public traits: Traits
 	) {
-		super();
+		super(traits);
 	}
 
 	getNativeMetadata(): MetadataInfo {

lib/abc/lazy/ClassTraitInfo.ts
@@ -8,10 +8,10 @@ export class ClassTraitInfo extends SlotTraitInfo {
 	constructor(
 		abc: ABCFile,
 		kind: TRAIT,
-		name: Multiname | number,
+		multiname: Multiname,
 		slot: number,
 		public classInfo: ClassInfo
 	) {
-		super(abc, kind, name, slot, 0, 0, -1);
+		super(abc, kind, multiname, slot, abc.getMultiname(0), 0, -1);
 	}
 }

lib/abc/lazy/ExceptionInfo.ts
@@ -13,28 +13,16 @@ export class ExceptionInfo {
 		public start: number,
 		public end: number,
 		public target: number,
-		public type: Multiname | number,
-		public varIndex: number
+		public multiname: Multiname,
+		public type: Multiname
 	) {
 		// ...
 	}
 
-	getType(): Multiname {
-		if (typeof this.type === 'number') {
-			this.type = this.abc.getMultiname(<number> this.type);
-		}
-		return <Multiname> this.type;
-	}
-
 	getTraits(): Traits {
-		if (!this._traits) {
-			const traits: TraitInfo [] = [];
-			if (this.varIndex) {
-				traits.push(new SlotTraitInfo(this.abc, TRAIT.Slot,this.abc.getMultiname(this.varIndex), 1, this.type, 0, 0));
-			}
-			this._traits = new Traits(traits);
-			this._traits.resolve();
-		}
+		if (!this._traits)
+			this._traits = new Traits([new SlotTraitInfo(this.abc, TRAIT.Slot,this.multiname, 1, this.type, 0, 0)]);
+
 		return this._traits;
 	}
 }

lib/abc/lazy/Info.ts
@@ -1,4 +1,10 @@
+import { Traits } from "./Traits";
 
 export class Info {
 
+constructor(
+	public traits: Traits
+	) {
+		// ...
+	}
 }

lib/abc/lazy/InstanceInfo.ts
@@ -25,7 +25,7 @@ export class InstanceInfo extends Info {
 		public initializer: MethodInfo | number,
 		public traits: Traits
 	) {
-		super();
+		super(traits);
 		this._interfaces = null;
 	}
 

lib/abc/lazy/MethodBodyInfo.ts
@@ -15,7 +15,7 @@ export class MethodBodyInfo extends Info {
 		public catchBlocks: ExceptionInfo [],
 		public traits: Traits
 	) {
-		super();
+		super(traits);
 	}
 
 	trace(writer: IndentingWriter) {

commit fff78c260e88bec7c735f75790cb91f9481722d7

Fix calling anonymous function stored in object
Fixes regression introduced in 8386d90

Progresses FPA World 1 Remix

lib/jit.ts
@@ -2126,7 +2126,7 @@ export class Context {
 	}
 
 	call(value: AXCallable, obj: ASObject, pp: any[], scope: Scope = null): any {
-		if (scope && value.methodInfo.trait.multiname.name == 'getDefinitionByName') {
+		if (scope && value.methodInfo?.trait?.multiname.name == 'getDefinitionByName') {
 			return this.getdefinitionbyname(scope, obj, pp);
 		}
 		validateCall(this.sec, value, pp.length);