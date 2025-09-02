0e8ddd34bfb7245c99d40f2d6adf7aeb6f15db90
Rob Bateman <rob@infiniteturtles.co.uk>
2025-09-02 13:37:45 +0100

    remove ASArray Proxy

--- lib/run/AXSecurityDomain.ts
+++ lib/run/AXSecurityDomain.ts
@@ -726,7 +726,7 @@ export class AXSecurityDomain {
 		this.prepareNativeClass('AXNamespace', 'Namespace', false);
 
 		const AXArray = this.prepareNativeClass('AXArray', 'Array', false);
-		//D(AXArray, 'axBox', axBoxPrimitive);
+		D(AXArray, 'axBox', axBoxPrimitive);
 		AXArray.tPrototype.$BgtoString = AXFunction.axBox(function () {
 			return this.value.toString();
 		});
