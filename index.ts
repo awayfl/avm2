23a8b0b2a8c663ca58f46f0e5e89f67707fc7182
Rob Bateman <rob@infiniteturtles.co.uk>
2026-04-28 19:52:31 +0100

    update version number in index.ts

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-console.debug("AwayFL - AVM2 - 0.2.235");
+console.debug("AwayFL - AVM2 - 0.2.236");
 
 // export {AVMAwayStage} from "./lib/AVMAwayStage";
 export { ASObject } from './lib/nat/ASObject';
