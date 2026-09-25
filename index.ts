424ba631a079a42fa915c759dff6d532ca95387a
Francis <francisperez19@gmail.com>
2026-09-25 21:42:59 +0800

    update version number in index.ts

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-console.debug("AwayFL - AVM2 - 0.2.239");
+console.debug("AwayFL - AVM2 - 0.2.240");
 
 // export {AVMAwayStage} from "./lib/AVMAwayStage";
 export { ASObject } from './lib/nat/ASObject';
